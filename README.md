6a7153fb88017ee93c4075d8cf1301af534f7521
Mil Lad <millad90s@gmail.com>
2020-06-01 20:23:57 +0430

    usage added to readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # NetHostMgmt
 This project used to store servers, owners, and list of ip address are used in a network. hope Network administrators enjoy and contribute to deveope and provide a practical software 
 :)
+
+Usage:
+# register new user:
+# method: url-encoded
+curl -X POST -d "username=m.saedi2&password=m.saedi2" localhost:8080/registeruser
+
+# login 
+# method: url-encoded
+curl  -X POST -d "username=m.saedi2&password=mypass123" localhost:8080/login
+
+ 
--- main.go
+++ main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+	router := gin.Default()
+	test(router)
+	ping(router)
+	Login(router)
+	UserInfo(router)
+	Register(router)
+	router.Run(API_PORT)
 
-	srv1 := Server{NameID: "tx01", CPU: "2"}
-	srv2 := Server{NameID: "tx02", CPU: "4"}
-	srv3 := Server{NameID: "tx03", CPU: "8"}
-	srv1.addnewtodb()
-	srv2.addnewtodb()
-	srv3.addnewtodb()
+	// srv1 := Server{NameID: "tx01", CPU: "2"}
+	// srv2 := Server{NameID: "tx02", CPU: "4"}
+	// srv3 := Server{NameID: "tx03", CPU: "8"}
+	// srv4 := Server{NameID: "tx04", CPU: "16", RAM: "16"}
+	// srv1.addnewtodb()
+	// srv2.addnewtodb()
+	// srv3.addnewtodb()
+	// srv4.addnewtodb()
 
-	owner1 := Owner{Username: "owner1"}
-	owner2 := Owner{Username: "owner2"}
-	owner1.addnewOwner()
-	owner2.addnewOwner()
+	// owner1 := Owner{Username: "owner1"}
+	// owner2 := Owner{Username: "owner2"}
+	// owner1.addnewOwner()
+	// owner2.addnewOwner()
 
-	fmt.Printf("%v 000000000000000", srv3)
-	fmt.Printf("%v", owner1.Servers)
+	// fmt.Printf("%v 000000000000000", srv3)
+	// fmt.Printf("%v", owner1.Servers)
 
-	srv1.AddServerToOwner(owner2)
-	srv2.AddServerToOwner(owner1)
+	// srv1.AddServerToOwner(owner2)
+	// srv2.AddServerToOwner(owner1)
+	// srv4.AddServerToOwner(owner1)
 
 	ListAllUsers()
 
